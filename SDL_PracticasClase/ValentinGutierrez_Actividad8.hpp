02cc089ba48305b915a342c74e5cb58f6a15ae77
Valen <Valen@DESKTOP-2OGFSQF>
2016-12-03 13:34:18 +0100

    Add project files.

---
+++ SDL_PracticasClase/ValentinGutierrez_Actividad8.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <stdlib.h>
+#include <map>
+
+
+class Fecha {
+private:
+public:
+	Fecha() {
+
+	}
+};
+
+class Hora {
+private:
+public:
+	Hora() {
+
+	}
+};
+
+class Agenda {
+private:
+	
+public:
+	Agenda() {
+	}
+	void insertarEvento();
+	void eliminarEvento();
+
+
+};
+
+void Agenda::insertarEvento() {
+
+}
+
+void Agenda::eliminarEvento() {
+
+}
